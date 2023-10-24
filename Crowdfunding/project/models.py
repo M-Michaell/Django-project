ae1c8302c3474e50de500b5eb83d07a8c2f0c156
M-Michaell <michaelmaherhalim@gmail.com>
2023-10-24 19:37:25 +0300

    craete Donation, report ,rating models

--- Crowdfunding/project/models.py
+++ Crowdfunding/project/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 
 # Create your models here.
+
+
+
+
+class User(models.Model):
+    pass
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
@@ -31,3 +40,43 @@ class Campaign(models.Model):
 
 
 
+
+class Donation (models.Model):
+    donation = models.DecimalField(max_digits=10,
+                                    decimal_places=2 ,
+                                   validators=[MinValueValidator(limit_value=5.00)],
+                                   default=5.00)
+    project = models.ForeignKey(Campaign,on_delete=models.CASCADE,related_name="donation")
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="donation")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
+class Report (models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE,related_name="reports")
+    project = models.ForeignKey(Campaign,on_delete=models.CASCADE,related_name="reports")
+    report_category =[("1","Flase Information"),
+                  ("2","Violenece"),
+                  ("3","Harassment"),
+                  ("4","spam"),
+                  ("5","Hate speech"),
+                  ("6","Nudity"),
+                  ("7","Terroism"),
+                  ("8","Something else")
+                  ]
+    report=models.CharField(max_length=50,choices=report_category,default="1")
+    report_comment=models.TextField(default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Rate(models.Model):
+    rate = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(5)
+        ])
+    projcet = models.ForeignKey(Campaign, on_delete=models.CASCADE, related_name="rate")
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="rate")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
