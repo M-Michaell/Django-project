366cc6e07dcb57b6270a671906a4afa04cd384af
mohammed mostafa <mm994225@gmail.com>
2023-10-24 18:38:31 +0300

    add category tag campaign model

--- Crowdfunding/project/admin.py
+++ Crowdfunding/project/admin.py
@@ -4,5 +4,5 @@ from . import models
 # Register your models here.
 admin.site.register(models.Category)
 admin.site.register(models.Campaign)
-
+admin.site.register(models.Tag)
 
--- Crowdfunding/project/models.py
+++ Crowdfunding/project/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.name}'
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
     def __str__(self):
         return f'{self.name}'
 
@@ -13,9 +17,10 @@ class Campaign(models.Model):
     title = models.CharField(max_length=100)
     detail = models.TextField(max_length=5000)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    images = models.ImageField(upload_to='project/images/', blank=True)
-    tags = ArrayField(models.CharField(max_length=200), blank=True)
     total_target = models.DecimalField(max_digits=10, decimal_places=2)
+    featured = models.BooleanField()
+    tags = models.ManyToManyField(Tag, blank=True)
+    # when maged add user i will add relation between project and user
     start_date = models.DateField()
     end_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -24,6 +29,5 @@ class Campaign(models.Model):
     def __str__(self):
         return f'{self.title}'
 
-    def get_image_url(self):
-        return f'/media/{self.images}'
+
 
