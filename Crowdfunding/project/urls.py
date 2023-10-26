70686da3765a46c4a9f34b3954e592fca189a3e6
ShazaShaban <shathashabanalberry@gmail.com>
2023-10-27 01:56:29 +0200

    create image class views, urls

--- Crowdfunding/project/urls.py
+++ Crowdfunding/project/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from project.views import home, CreateImage, ImageView
 # from project.views import CreateCampaign
 
 urlpatterns = [
     # path('create/', CreateCampaign.as_view(), name="project.create"),
+    path('home/', home, name = 'project.home'),
+    path('uploadImage/', CreateImage.as_view(), name='images.create'),
+    path('viewImage/', ImageView.as_view(), name='images.show'),
 ]
--- Crowdfunding/project/views.py
+++ Crowdfunding/project/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from project.models import Image
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import DetailView
+from project.form import CustomizedImageCreationForm
+from django.urls import reverse_lazy, reverse
+
+
 # from django.views.generic.edit import CreateView
 # from project.models import Campaign
 # from project.form import CreateModelForm
@@ -8,3 +15,44 @@ from django.shortcuts import render
 #     model = Campaign
 #     template_name = 'project/create.html'
 #     form_class = CreateModelForm
+
+
+def home(request):
+    return render(request, 'project/home.html')
+
+class CreateImage(CreateView):
+    model = Image
+    template_name = 'project/create.html'
+    form_class = CustomizedImageCreationForm
+    success_url = reverse_lazy("project.home")
+
+    def form_valid(self, form):
+        # Determine which button was clicked
+        if 'save' in self.request.POST:
+            self.success_url = reverse_lazy('project.home')
+        elif 'add' in self.request.POST:
+            self.success_url = reverse_lazy('images.create')
+
+        return super().form_valid(form)
+
+
+class DeleteImage(DeleteView):
+    model = Image
+    template_name = 'accounts/delete.html'
+    success_url = reverse_lazy('project.home')
+
+class EditProfileView(UpdateView):
+   model = Image
+   template_name = 'project/edit.html'
+   form_class = CustomizedImageCreationForm
+   success_url = reverse_lazy('project.home')
+
+class ImageView(DetailView):
+    model = Image
+    template_name = 'project/view.html'
+    context_object_name = 'image'
+    def get_object(self, queryset=None):
+        return self.request.user
+    
+
+    
