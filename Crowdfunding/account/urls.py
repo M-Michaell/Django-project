f01d9d804281eab4dc9ff14480a1ef083c67da0c
MagedKhaled <maged.khaled03@gmail.com>
2023-10-27 16:24:10 +0200

    activation link is ready, lack make it limited for 24 hours

--- Crowdfunding/account/urls.py
+++ Crowdfunding/account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from account.views import CustomRegistrationView, CustomLoginView, userHome,CustomLogoutView,CustomEditAccountView,send_email
+from account.views import CustomRegistrationView, CustomLoginView, userHome,CustomLogoutView,CustomEditAccountView,activate
 
 urlpatterns = [
     path('home/', userHome, name='account.home'),
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='account.login'),
     path('logout/', CustomLogoutView.as_view(), name='account.logout'),
     path('edit/', CustomEditAccountView.as_view(), name='account.edit'),
-    path('send_email/', send_email, name='account.send_email'),
+    path('activate/<str:uidb64>/<str:token>/', activate, name='activate'),
+
 ]
 
--- Crowdfunding/account/views.py
+++ Crowdfunding/account/views.py
@@ -6,7 +6,15 @@ from django.contrib.auth import login
 from django.contrib import messages
 from django.views.generic.edit import CreateView, UpdateView
 from account.models import CustomUser
-
+from django.contrib.sites.shortcuts import get_current_site
+from account.tokens import account_activation_token
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.views.generic.edit import CreateView
+from django.utils.http import urlsafe_base64_decode
+from django.http import HttpResponse
 
 
 def userHome(request):
@@ -22,8 +30,38 @@ class CustomRegistrationView(CreateView):
     success_url = reverse_lazy('account.login') 
 
     def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        current_site = get_current_site(self.request)
+        mail_subject = 'Activate your blog account.'
+        message = render_to_string('account/acc_active_email.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        })
+        to_email = form.cleaned_data.get('email')
+        email = EmailMessage(mail_subject, message, to=[to_email])
+        email.send()
+        
         messages.success(self.request, 'Account data created successfully.')
         return super().form_valid(form)
+    
+def activate(request, uidb64, token):
+    # try:
+    uid = urlsafe_base64_decode(uidb64)
+    user = CustomUser.objects.get(pk=uid)
+    # except(TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+        # user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
     
 
 class CustomEditAccountView(UpdateView):
@@ -63,36 +101,3 @@ class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('account.login') 
 
 
-
-from django.core.mail import send_mail
-from django.http import JsonResponse
-
-def send_email(request):
-    admin_email = 'emailer.iti@gmail.com'
-    message_to_send = {
-        'subject': 'Test email from Django',
-        'user_email': 'maged.khaled03@gmail.com',
-        'body': 'Hello there',
-    }
-
-    # try:
-    print('Try sending')
-    sent_count = send_mail(
-        subject=message_to_send['subject'],
-        message=message_to_send['body'],
-        from_email=admin_email,
-        recipient_list=[message_to_send['user_email']],
-        fail_silently=False,  # Set to False to catch email sending errors
-    )
-
-    if sent_count > 0:
-        return JsonResponse({'status': 'Email sent successfully.'})
-    else:
-        return JsonResponse({'status': 'Email failed to send.'})
-
-    # except Exception as e:
-    #     return JsonResponse({'status': f'Email sending error: {str(e)}'})
-
-
-
-# p='emailer.iti@2023'
