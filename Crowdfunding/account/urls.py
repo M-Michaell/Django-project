830b43545ff08720b8d1bc15263ca03cddeaec1c
MagedKhaled <maged.khaled03@gmail.com>
2023-10-27 15:11:29 +0200

    send email using google account is ready, lack generate activation link

--- Crowdfunding/account/urls.py
+++ Crowdfunding/account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from account.views import CustomRegistrationView, CustomLoginView, userHome,CustomLogoutView,CustomEditAccountView
+from account.views import CustomRegistrationView, CustomLoginView, userHome,CustomLogoutView,CustomEditAccountView,send_email
 
 urlpatterns = [
     path('home/', userHome, name='account.home'),
     path('register/', CustomRegistrationView.as_view(), name='account.register'),
     path('login/', CustomLoginView.as_view(), name='account.login'),
     path('logout/', CustomLogoutView.as_view(), name='account.logout'),
-    path('edit', CustomEditAccountView.as_view(), name='account.edit')
+    path('edit/', CustomEditAccountView.as_view(), name='account.edit'),
+    path('send_email/', send_email, name='account.send_email'),
 ]
 
--- Crowdfunding/account/views.py
+++ Crowdfunding/account/views.py
@@ -9,7 +9,6 @@ from account.models import CustomUser
 
 
 
-
 def userHome(request):
     return render(request, 'account/home.html')
 
@@ -37,7 +36,7 @@ class CustomEditAccountView(UpdateView):
         if 'image' in form.cleaned_data:
             self.object.image = form.cleaned_data['image']
             self.object.save()
-            
+
         messages.success(self.request, 'Account data updated successfully.')
         return super().form_valid(form)
     
@@ -64,3 +63,36 @@ class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('account.login') 
 
 
+
+from django.core.mail import send_mail
+from django.http import JsonResponse
+
+def send_email(request):
+    admin_email = 'emailer.iti@gmail.com'
+    message_to_send = {
+        'subject': 'Test email from Django',
+        'user_email': 'maged.khaled03@gmail.com',
+        'body': 'Hello there',
+    }
+
+    # try:
+    print('Try sending')
+    sent_count = send_mail(
+        subject=message_to_send['subject'],
+        message=message_to_send['body'],
+        from_email=admin_email,
+        recipient_list=[message_to_send['user_email']],
+        fail_silently=False,  # Set to False to catch email sending errors
+    )
+
+    if sent_count > 0:
+        return JsonResponse({'status': 'Email sent successfully.'})
+    else:
+        return JsonResponse({'status': 'Email failed to send.'})
+
+    # except Exception as e:
+    #     return JsonResponse({'status': f'Email sending error: {str(e)}'})
+
+
+
+# p='emailer.iti@2023'
