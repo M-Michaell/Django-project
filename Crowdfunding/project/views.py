9f575dab5dbd396807c50c063648ac622799d3dd
M-Michaell <michaelmaherhalim@gmail.com>
2023-10-28 23:28:42 +0200

    add messages and rating

--- Crowdfunding/project/views.py
+++ Crowdfunding/project/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render ,redirect 
 from django.template import loader
 from django.urls import reverse_lazy ,reverse
+from django.http import Http404
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from django.db.models import Sum, Count,Avg
@@ -47,6 +48,7 @@ class CreateTag(CreateView):
         return super().form_valid(form)
 
 
+from django.contrib.messages import add_message, constants as messages
 
 def campaign_details(request, campaign_id):
     try:
@@ -59,7 +61,7 @@ def campaign_details(request, campaign_id):
     comments = campaign.comments.all()
     tags = campaign.tags.all()
     images_all = campaign.image.all()
-    rating = campaign.rate.aggregate(rate=Avg('rate'))['rate'] or 0.00
+    rating = round(campaign.rate.aggregate(rate=Avg('rate'))['rate'] or 0.00 ,2)
     related_campaigns = Campaign.objects.filter(tags__in=tags).exclude(pk=campaign_id).distinct()[:4]
     progress = (total_donation / campaign.total_target) * 100
     last_donation = Donation.objects.filter(campaign=campaign).last().created_at
@@ -71,26 +73,27 @@ def campaign_details(request, campaign_id):
     comment_report_form =CreateCommentReportForm()
     create_rate =CreateRatingForm()
 
-
-
+    error_message = None
     context = {
-        'campaign': campaign,
-        'total_donation': total_donation,
-        'donation_count': donation_count,
-        'comments': comments,
-        'tags': tags,
-        'rating': rating * 20,
-        'f_image': images_all[0],
-        'images': images_all[1:],
-        'related_campaigns': related_campaigns,
-        "progress": progress,
-        'comment_form': comment_form,
-        'donation_form': donation_form,
-        'report_form': report_form,
-        "comment_report_form":comment_report_form,
-        'last_donation': last_donation,
-        "create_rate":create_rate
-    }
+    'campaign': campaign,
+    'total_donation': total_donation,
+    'donation_count': donation_count,
+    'comments': comments,
+    'tags': tags,
+    'rating_width': rating * 20,
+    'f_image': images_all[0],
+    'images': images_all[1:],
+    'related_campaigns': related_campaigns,
+    "progress": progress,
+    'comment_form': comment_form,
+    'donation_form': donation_form,
+    'report_form': report_form,
+    "comment_report_form":comment_report_form,
+    'last_donation': last_donation,
+    "create_rate":create_rate,
+    "rating" :rating,
+    "error_message": error_message 
+}
 
     if request.method == 'POST':
         if 'comment_submit' in request.POST:
@@ -100,6 +103,7 @@ def campaign_details(request, campaign_id):
                 comment.campaign = campaign
                 comment.user = request.user
                 comment.save()
+                add_message(request, messages.SUCCESS, "Your comment has been successfully added.")
                 return redirect('campaign.details', campaign_id=campaign_id)
 
         elif 'donation_submit' in request.POST:
@@ -109,6 +113,10 @@ def campaign_details(request, campaign_id):
                 donation.campaign = campaign
                 donation.user = request.user
                 donation.save()
+                add_message(request, messages.SUCCESS, "Your donation has been successfully processed.")
+                return redirect('campaign.details', campaign_id=campaign_id)
+            else:
+                add_message(request, messages.ERROR, "Your donation should be geater than 5 .")
                 return redirect('campaign.details', campaign_id=campaign_id)
 
             
@@ -119,6 +127,7 @@ def campaign_details(request, campaign_id):
                 report.campaign = campaign
                 report.user = request.user
                 report.save()
+                add_message(request, messages.SUCCESS, "Your report has been successfully submitted.")
                 return redirect('campaign.details', campaign_id=campaign_id)
             
         elif 'comment_report' in request.POST:
@@ -129,19 +138,24 @@ def campaign_details(request, campaign_id):
                 report.comment = Comment.objects.get(id=comment_id)
                 report.user = request.user
                 report.save()
+                add_message(request, messages.SUCCESS, "Your comment report has been successfully submitted.")
                 return redirect('campaign.details', campaign_id=campaign_id)
             
 
         elif 'rate_submit' in request.POST:
             create_rate = CreateRatingForm(request.POST)
             if create_rate.is_valid():
-                rate = create_rate.save(commit=False)
-                rate.user = request.user
-                rate.campaign = campaign
-                rate.save()
-                return redirect('campaign.details', campaign_id=campaign_id)
-            
+                user_rating = Rate.objects.filter(campaign=campaign, user=request.user).first()
 
+                if user_rating:
+                    add_message(request, messages.ERROR, "You have already rated this campaign before.")
+                else:
+                    rate = create_rate.save(commit=False)
+                    rate.user = request.user
+                    rate.campaign = campaign
+                    rate.save()
+                    add_message(request, messages.SUCCESS, "Your rating has been successfully added.")
+                    return
 
 
 
